a832b8934da4ab7799943febfc973fc6cdfd9403
hazelclein <hazel27cm@gmail.com>
2025-12-11 11:44:47 +0700

    3

--- todo/main.go
+++ todo/main.go
@@ -9,7 +9,6 @@ import (
 
 func main() {
 	var todos []string
-
 	var pilihan int
 	reader := bufio.NewReader(os.Stdin)
 
@@ -17,15 +16,17 @@ func main() {
 		fmt.Println("=====/selamat datang di app todo/=====")
 		fmt.Println("1. Create Todo")
 		fmt.Println("2. Look Todo")
-		fmt.Println("3. Hapus Todo")
-		fmt.Println("4. Keluar")
+		fmt.Println("3. Edit Todo")
+		fmt.Println("4. Hapus Todo")
+		fmt.Println("5. Keluar")
 
-		fmt.Println("Pilih menu (1-4): ")
+		fmt.Print("Pilih menu (1-5): ")
 		fmt.Scanln(&pilihan)
 
 		switch pilihan {
 
 		case 1:
+			// Create Todo
 			fmt.Print("Masukan Todo: ")
 			todo, _ := reader.ReadString('\n')
 			todo = strings.TrimSpace(todo)
@@ -34,6 +35,7 @@ func main() {
 			fmt.Println("Todo Berhasil di tambahkan")
 
 		case 2:
+			// Look Todo
 			fmt.Println("\nDaftar Todo")
 
 			if len(todos) == 0 {
@@ -45,6 +47,34 @@ func main() {
 			}
 
 		case 3:
+			// Edit Todo
+			if len(todos) == 0 {
+				fmt.Println("\nTidak ada Todo yang bisa di edit")
+				break
+			}
+
+			fmt.Println("\nDaftar Todo: ")
+			for i, todo := range todos {
+				fmt.Printf("%d. %s\n", i+1, todo)
+			}
+
+			var nomor int
+			fmt.Print("Edit Todo No: ")
+			fmt.Scanln(&nomor)
+
+			if nomor < 1 || nomor > len(todos) {
+				fmt.Println("Nomor tidak valid!!")
+			} else {
+				fmt.Print("Masukan todo baru: ")
+				todoBaru, _ := reader.ReadString('\n')
+				todoBaru = strings.TrimSpace(todoBaru)
+
+				todos[nomor-1] = todoBaru
+				fmt.Println("Todo Berhasil di edit!!")
+			}
+
+		case 4:
+			// Hapus Todo
 			if len(todos) == 0 {
 				fmt.Println("\nTidak ada Todo yang bisa di hapus")
 				break
@@ -63,10 +93,11 @@ func main() {
 				fmt.Println("Nomor tidak valid!!")
 			} else {
 				todos = append(todos[:nomor-1], todos[nomor:]...)
-				fmt.Println("Todo Berhasil di hapus!!")
+				fmt.Println("🗑Todo Berhasil di hapus!!")
 			}
 
-		case 4:
+		case 5:
+			// Keluar
 			fmt.Println("THANK UUU")
 			return
 
